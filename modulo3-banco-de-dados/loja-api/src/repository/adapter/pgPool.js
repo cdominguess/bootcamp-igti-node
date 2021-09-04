666b72bd9cfe05752614e0c6241a6b327c84244f
Carlos F. Domingues <dominguess@gmail.com>
2021-09-03 23:58:02 -0300

    Método para montar dinamicamente dados de UPDATE para uma entidade

--- modulo3-banco-de-dados/loja-api/src/repository/adapter/pgPool.js
+++ modulo3-banco-de-dados/loja-api/src/repository/adapter/pgPool.js
@@ -7,14 +7,12 @@ class PgPool {
         const objPool = new pg.Pool({
             connectionString: `postgres://${objConfig.user}:${objConfig.password}@${objConfig.host}/${objConfig.database}`
         });
-
         this.objConexao = objPool;
     }
 
     async criar(obj, entidade) {
         try {
             const dadosInsert = this._montarDadosInsert(obj, entidade);
-
             const conn = await this.objConexao.connect();
             const res = await conn.query(dadosInsert.sql, dadosInsert.valores);
 
@@ -26,6 +24,29 @@ class PgPool {
 
     /**
      * Método que monta dinamicamente o necessário para um INSERT, conforme dados de "obj" e tabela de "entidade"
+     * @param {object} obj 
+     * @param {string} entidade 
+     * @returns object
+     */
+    _montarDadosInsert(obj, entidade) {
+        const arrColunas = Object.keys(obj);
+        const arrValores = Object.values(obj);
+        const countColunas = arrColunas.length;
+
+        let strInsert = `INSERT INTO ${entidade} (${arrColunas.join(', ')}) VALUES (`;
+        for (let i = 1; i <= countColunas; i++) {
+            strInsert += ((i < countColunas) ? `$${i}, ` : `$${i}`);
+        }
+        strInsert += `) RETURNING *;`;
+
+        return {
+            sql: strInsert,
+            valores: arrValores
+        }
+    }
+
+    /**
+     * Método que monta dinamicamente o necessário para um UPDATE, conforme dados de "obj" e tabela de "entidade"
      * @param {object} obj 
      * @param {string} entidade 
      * @returns object
